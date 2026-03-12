dfa264617eaaa8c9b3a493317d28bc28a9d6a125
Dmitry Tretyakov <tinelix@mail.ru>
2026-03-12 10:20:46 +0700

    Update year in copyright notice

--- app/src/main/java/uk/openvk/android/legacy/ui/views/IncomingMessageLayout.java
+++ app/src/main/java/uk/openvk/android/legacy/ui/views/IncomingMessageLayout.java
@@ -1,6 +1,6 @@
 /*
- *  Copyleft © 2022, 2023, 2024 OpenVK Team
- *  Copyleft © 2022, 2023, 2024 Dmitry Tretyakov (aka. Tinelix)
+ *  Copyleft © 2022-24, 2026 OpenVK Team
+ *  Copyleft © 2022-24, 2026 Dmitry Tretyakov (aka. Tinelix)
  *
  *  This file is part of OpenVK Legacy for Android.
  *
